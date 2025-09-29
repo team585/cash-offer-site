2330a1918bf4510046514edf4377a22478079c92
team585 <team@eaglereproperties.net>
2025-09-29 14:52:35 -0500

    thank-you.vue

---
+++ pages/thank-you.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="max-w-2xl mx-auto p-10 text-center">
+    <h1 class="text-3xl font-bold mb-4">Thank You! We’ve Got Your Request.</h1>
+    <p class="mb-6">
+      Our team will reach out to you within 24–48 hours with your cash offer.
+    </p>
+  </section>
+</template>
